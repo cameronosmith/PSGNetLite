a1f1b11139242c1559c847af6aa85f23f7b94812
cameronosmith <cos008@ucsd.edu>
2021-04-13 15:21:57 -0700

    No thresholding

--- LabelProp.py
+++ LabelProp.py
@@ -6,15 +6,16 @@ from torch_scatter import scatter_max
 
 # Takes in number of nodes, the graph's edge index (adj. list), number of LP
 # iterations, and returns the label for each node 
-def LP_clustering(num_nodes,edge_index,num_iter=10,device="cuda"):
+def LP_clustering(num_nodes,edge_index,edge_weight,num_iter=10,device="cuda"):
 
     # Sparse adjacency matrix
     adj_t = SparseTensor(row=edge_index[0], col=edge_index[1],
-                         sparse_sizes=(num_nodes, num_nodes)).t().to(device)
+           sparse_sizes=(num_nodes, num_nodes),value=edge_weight).t().to(device)
 
     # Each node starts with its index as its label
     x = SparseTensor.eye(num_nodes).to(device)
 
+    # Run LP for num_iter iterations
     for _ in range(num_iter):
 
         # Add each node's neighbors' labels to its list of neighbor nodes
